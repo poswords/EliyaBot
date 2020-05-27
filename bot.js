06a0be810b2fe52bb86d52646139ff309ea68f65
poswords <poswords@gmail.com>
2020-05-28 00:36:31 +0900

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,17 @@
+// Run dotenv
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
