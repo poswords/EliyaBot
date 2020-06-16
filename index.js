27e0b9d7ee8a954e8e2473b51ddfa180e4838c23
JPSkenp <ken.p@d-jps.com>
2020-06-16 14:27:50 +0900

    Update index.js

--- index.js
+++ index.js
@@ -161,8 +161,8 @@ io.on('connection', function (socket) {
 
 		client.connect();
 
-		client.query("INSERT INTO short_urls (url,equips) VALUES ('" + list.chars + "', '" + list.equips + "' RETURNING id", (err, res) => {
-			console.log(res)
+		client.query("INSERT INTO short_urls (url,equips) VALUES ('" + list.chars + "', '" + list.equips + "') RETURNING id", (err, res) => {
+			
 		  if (err) throw err;
 		  for (let row of res.rows) {
 		
